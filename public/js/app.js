eb23536dbdb17cae2e39fd1c9477d4a718cc9483
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-06-06 00:58:34 +0700

    using compiled template to get full rendered markup with variables and callable defined methods

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,7 @@
-createPage = (name) => {
+createPage = (name, object = {}, methods = {}) => {
     return Vue.component('page-' + name, {
-        data: () => {
-            return  {
-              content: ''
-            }
+        data: function () {
+            return Object.assign({'content': ''}, object);;
         },
         mounted () {
             (new Promise( (resolve) => {
@@ -18,13 +16,24 @@ createPage = (name) => {
                 ).then(response =>  resolve(response.text()));
             })).then(result => this.content = result);
         },
-        template: '<div v-html="content"></div>'
+        render: function (c) {
+            return c(Vue.compile('<div>' + this.content + '</div>'));
+        },
+        methods: methods
     });
 }
 
 const routes = [
     { path: '/', component: createPage('home') },
-    { path: '/about', component: createPage('about') },
+    { path: '/about', component: createPage(
+        'about',
+        {
+            name: 'Abdul Malik Ikhsan'
+        },
+        {
+            hit: () => alert('I am a web developer!')
+        }
+    )},
     { path: '/contact', component: createPage('contact') },
     { path: "*", component: createPage('404') }
 ];
@@ -35,9 +44,14 @@ const router = new VueRouter({
     mode: 'history',
     linkExactActiveClass: "active"
 });
-const app    = new Vue({router}).$mount('#root')
+
+vue = new Vue({
+    router
+}).$mount('#root');
 
 // https://vuejs.org/v2/guide/
 // https://router.vuejs.org/guide/#html
 // https://medium.com/badr-interactive/mengenal-lifecycle-hooks-pada-vue-js-78cd2225a69
 // https://forum.vuejs.org/t/setting-a-correct-base-url-with-vue-router/24726/2
+// https://vuejs.org/v2/guide/render-function.html#Functional-Components
+// https://stackoverflow.com/questions/51548729/vuejs-vue-app-render-method-with-dynamic-template-compiled-is-throwing-some/51552701
