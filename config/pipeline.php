f578b31734eb72dce739939e71bb15f8027e8652
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-06-11 19:49:28 +0700

    add access 404 page handling

--- config/pipeline.php
+++ config/pipeline.php
@@ -70,9 +70,4 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
     // Register the dispatch middleware in the middleware pipeline
     $app->pipe(XMLHttpRequestTemplateMiddleware::class);
     $app->pipe(DispatchMiddleware::class);
-
-    // At this point, if no Response is returned by any middleware, the
-    // NotFoundHandler kicks in; alternately, you can provide other fallback
-    // middleware to execute.
-    $app->pipe(NotFoundHandler::class);
 };
