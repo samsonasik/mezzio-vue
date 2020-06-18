e2884b1c897d155aab2ffa3581a57f20eb163d3a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-06-18 14:21:20 +0700

    add AboutPage test

---
+++ test/AppTest/Unit/Handler/AboutPageHandlerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Unit\Handler;
+
+use App\Handler\AboutPageHandler;
+use Laminas\Diactoros\Response\HtmlResponse;
+use Mezzio\Template\TemplateRendererInterface;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Psr\Http\Message\ServerRequestInterface;
+
+class AboutPageHandlerTest extends TestCase
+{
+    use ProphecyTrait;
+
+    public function testReturnsHtmlResponse()
+    {
+        $renderer = $this->prophesize(TemplateRendererInterface::class);
+
+        ob_start();
+        require_once 'src/App/templates/app/about-page.phtml';
+        $content = ob_get_clean();
+
+        $renderer
+            ->render('app::about-page')
+            ->willReturn($content);
+
+        $page = new AboutPageHandler(
+            $renderer->reveal()
+        );
+
+        $response = $page->handle(
+            $this->prophesize(ServerRequestInterface::class)->reveal()
+        );
+
+        $this->assertInstanceOf(HtmlResponse::class, $response);
+    }
+}
