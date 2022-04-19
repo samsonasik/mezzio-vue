06fe0bb29bc9a3af4263d00b5648f78abbc8fa7c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-20 05:39:20 +0700

    config

---
+++ rector.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/test',
+    ]);
+    $rectorConfig->skip([
+        __DIR__ . '/src/App/templates',
+        AbsolutizeRequireAndIncludePathRector::class,
+    ]);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_74,
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+    ]);
+};
